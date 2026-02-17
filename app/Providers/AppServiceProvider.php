c193f4a3e40324494cd63cfcf652213ee80c16f5
juandatorr10 <juandatorr@gmail.com>
2026-02-17 18:59:13 -0500

    Fix: Habilitar acceso público a Log Viewer en producción

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Opcodes\LogViewer\Facades\LogViewer;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
         //
         if (app()->environment('production')) {
             URL::forceScheme('https');
+            LogViewer::auth(function ($request) {
+                return true;
+            });
         }
     }
 }
